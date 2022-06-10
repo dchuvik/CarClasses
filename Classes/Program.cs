25ded8c8ff779470e5c473e4204d8865b65030d7
David Chuvik <dchuvik@gmail.com>
2022-06-10 12:43:52 -0400

    Made 2 new Cars

---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Toyota", "Camry", 2009);
+            Console.WriteLine(car1.Make);
+            Console.WriteLine(car1.Model);
+            Console.WriteLine(car1.Year); 
+            Console.WriteLine();
+
+            Car car2 = new Car("Honda", "Civic", 2020);
+            Console.WriteLine(car2.Make);
+            Console.WriteLine(car2.Model);
+            Console.WriteLine(car2.Year);
+            Console.WriteLine();
+
+        }
+    }
+}
